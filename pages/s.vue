0bbc6712ef35a988ff50770fc2117a06b412d8bb
SimonGioffredi <simon@spin-interactive.com>
2023-03-28 14:23:05 +0200

    :sparkles: 2046 | TplSearchResults done

--- pages/s.vue
+++ pages/s.vue
@@ -1,10 +1,36 @@
 <template>
-    <TplSearch />
+    <TplSearchResults :items="page.list" />
 </template>
 
 <script>
+    import { displayError } from '@/utils/devErrorHandle'
+
     export default {
         name: 'RouteSearch',
+        async asyncData({ route, $api, error }) {
+            try {
+                const params = {
+                    ...route.params,
+                    ...route.query,
+                }
+
+                const { data } = await $api.get('search', {
+                    params,
+                })
+
+                return {
+                    page: data,
+                }
+            } catch (err) {
+                displayError({ err })
+                return error({
+                    statusCode: 404,
+                })
+            }
+        },
+        mounted() {
+            console.log(this.page)
+        },
     }
 </script>
 
