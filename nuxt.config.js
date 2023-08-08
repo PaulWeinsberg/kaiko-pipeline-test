f356d9a612d2f7da111bb30d61abcdb7be1e0ed0
SimonGioffredi <simon@spin-interactive.com>
2023-08-08 15:32:34 +0200

    :package: @spin-interactive/spikotify -> 1.6.5

--- nuxt.config.js
+++ nuxt.config.js
@@ -149,7 +149,7 @@ export default {
         transpile: ['gsap', 'axios'],
         loaders: { scss: { sourceMap: false } },
         extend(config) {
-            config.resolve.alias.vue$ = 'vue/dist/vue.esm.js'
+            config.resolve.alias.vue = 'vue/dist/vue.common'
         },
     },
 
