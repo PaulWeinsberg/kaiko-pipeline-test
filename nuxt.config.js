757ebf79165d94f6e9848767a64a05299fc11dd7
SimonGioffredi <simon@spin-interactive.com>
2023-08-21 13:02:24 +0200

    :bug: 3475 | Remove nuxt-hilhigts.js to do it by our own hand

--- nuxt.config.js
+++ nuxt.config.js
@@ -119,13 +119,6 @@ export default {
         '@nuxtjs/sitemap',
         // https://image.nuxtjs.org
         '@nuxt/image',
-        // https://www.npmjs.com/package/nuxt-highlightjs
-        [
-            'nuxt-highlightjs',
-            {
-                style: 'night-owl',
-            },
-        ],
     ],
 
     // Style resources
