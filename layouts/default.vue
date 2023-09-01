3ebac8af999403a9823d16d0655293d81342c922
SimonGioffredi <simon@spin-interactive.com>
2023-09-01 12:06:58 +0200

    :ambulance: Remove fake mainteance page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,39 @@
 <template>
     <div id="app" class="default">
-        <!--        <BaseHeader :search="showSearchField" :has-mega-menu="false" />-->
-        <!--        <BaseMegaMenu />-->
-        <!--        <Nuxt />-->
-        <!--        <BaseFooter-->
-        <!--            :footer="footer"-->
-        <!--            :sub-footer="subFooter"-->
-        <!--            :options="options"-->
-        <!--        />-->
+        <BaseHeader :search="showSearchField" :has-mega-menu="false" />
+        <BaseMegaMenu />
+        <Nuxt />
+        <BaseFooter
+            :footer="footer"
+            :sub-footer="subFooter"
+            :options="options"
+        />
     </div>
 </template>
 
 <script>
-    // import { mapState } from 'vuex'
-    // import layoutMixin from '@/mixins/layoutMixin'
+    import { mapState } from 'vuex'
+    import layoutMixin from '@/mixins/layoutMixin'
 
     export default {
         name: 'LayoutDefault',
-        // mixins: [layoutMixin],
-        // computed: {
-        //     ...mapState({
-        //         header: state => state.menu.menu,
-        //         footer: state => state.menu.footer.items,
-        //         subFooter: state => state.menu.footerSub.items,
-        //         options: state => state.options.options,
-        //     }),
-        //     showSearchField() {
-        //         const { header } = this
-        //         return header?.custom_fields?.show_search_field ?? false
-        //     },
-        // },
-        // watch: {
-        //     $route() {
-        //         this.$nuxt.$emit('navigation:show', { show: false })
-        //     },
-        // },
+        mixins: [layoutMixin],
+        computed: {
+            ...mapState({
+                header: state => state.menu.menu,
+                footer: state => state.menu.footer.items,
+                subFooter: state => state.menu.footerSub.items,
+                options: state => state.options.options,
+            }),
+            showSearchField() {
+                const { header } = this
+                return header?.custom_fields?.show_search_field ?? false
+            },
+        },
+        watch: {
+            $route() {
+                this.$nuxt.$emit('navigation:show', { show: false })
+            },
+        },
     }
 </script>
