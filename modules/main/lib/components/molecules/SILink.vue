4e9d75ea449522384dbf59e11683d98f076eee23
Paul Weinsberg <p.weinsberg@icloud.com>
2026-03-31 08:29:48 +0200

    WP-47
    - Fixes double events race in some cases by using mouseup instead of click

--- modules/main/lib/components/molecules/SILink.vue
+++ modules/main/lib/components/molecules/SILink.vue
@@ -6,9 +6,11 @@
         :target="_target"
         rel="noopener"
         @click="onClick"
+        @mouseup="handleMouseUp"
+
     >
         <SIIcon v-if="icon && iconPosition === 'left'" :name="icon" />
-        <span v-if="title" class="si-link-title" @click="handleLinkClick">{{ title }}</span>
+        <span v-if="title" class="si-link-title">{{ title }}</span>
         <slot />
         <SIIcon v-if="icon && iconPosition === 'right'" :name="icon" />
     </a>
@@ -17,15 +19,20 @@
         :class="classNames"
         :to="to"
         :target="_target"
+        @mouseup="handleMouseUp"
     >
         <SIIcon v-if="icon && iconPosition === 'left'" :name="icon" />
-        <span v-if="title" class="si-link-title" @click="handleLinkClick">{{ title }}</span>
+        <span v-if="title" class="si-link-title">{{ title }}</span>
         <slot />
         <SIIcon v-if="icon && iconPosition === 'right'" :name="icon" />
     </NuxtLink>
-    <component :is="tag" v-else :class="classNames">
+    <component
+        :is="tag" v-else
+        :class="classNames"
+        @mouseup="handleMouseUp"
+    >
         <SIIcon v-if="icon && iconPosition === 'left'" :name="icon" />
-        <span v-if="title" class="si-link-title" @click="handleLinkClick">{{ title }}</span>
+        <span v-if="title" class="si-link-title">{{ title }}</span>
         <slot />
         <SIIcon v-if="icon && iconPosition === 'right'" :name="icon" />
     </component>
@@ -203,13 +210,14 @@
         methods: {
             onClick(e) {
                 const { isAnchor, path_ } = this
+                this.handleMouseUp();
                 if (!isAnchor) return false
                 e.preventDefault()
                 scrollTo(path_, {
                     toVars: { duration: 0.3 },
                 })
             },
-            handleLinkClick() {
+            handleMouseUp() {
                 this.$emit('click:link')
             },
         },
